a4d44a3309995be504250124f6856da30b8f36de
Rick Rieger <rickrieger36251@gmail.com>
2021-07-08 18:52:50 -0400

    first commit

---
+++ routes/twilio/twilioRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const jwtMiddleware = require("../utils/jwtMiddleware");
+const client = require("twilio")(accountSid, authToken);
+//jwtMiddleware protects us from others using our app
+router.post("/send-sms",jwtMiddleware, function (req, res) {
+  console.log(`+1${req.body.number}`);
+  client.messages
+    .create({
+      body: req.body.message,
+      from: "+19165849590",
+      to: `+1${req.body.number}`,
+    })
+    .then((message) => res.json(message))
+    .catch((error) => {
+      console.log(error.message);
+      res.status(error.status).json({ message: error.message, error });
+    });
+});
+module.exports = router;
